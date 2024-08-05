e71ee72082bbe4d761e72c357614f2e8a6cda951
AdorableParker <jet.piane.2010@qq.com>
2024-08-05 20:22:39 +0800

    修复任意原料为0时的计算bug

--- src/main/kotlin/craft/Doping.kt
+++ src/main/kotlin/craft/Doping.kt
@@ -9,12 +9,19 @@ abstract class Doping : Craft() {
     override val name: String = "交糅工艺"
         get() = field + Tools.intToRoman(objId)
 
-    fun routine(pet: Int, shay: Int): Pair<Int, Int> {
-        val remainingShay = shay - pet
-        return if (remainingShay >= 0) Pair(pet, remainingShay) else Pair(shay, 0)
+    /** 常规
+     * case 1. pet > auxiliary => pet^: auxiliary pet: pet-auxiliary auxiliary: 0
+     * case 2. pet < auxiliary => pet^: pet pet: 0 auxiliary: auxiliary-pet
+     * case 3. pet = auxiliary => pet^: pet pet: 0 auxiliary: 0
+     * */
+    fun routine(pet: Int, auxiliary: Int): Triple<Int, Int, Int> = when {
+        pet > auxiliary -> Triple(auxiliary, pet - auxiliary, 0)
+        pet < auxiliary -> Triple(pet, auxiliary - pet, 0)
+        else -> Triple(auxiliary, 0, 0)
     }
 
-    fun equalization(pet: Int, shay: Int): Pair<Int, Int> = ceil((pet + shay) * 0.5).toInt() to 0
+    /** 等分 */
+    fun equalization(pet: Int, shay: Int): Triple<Int, Int, Int> = Triple(ceil((pet + shay) * 0.5).toInt(), 0, 0)
 
 }
 
@@ -25,11 +32,16 @@ class DopingI(override val level: Int) : Doping() {
         val petCount = material.find { it is PetIner }?.count ?: 0
         val shayCount = material.find { it is ShayIner }?.count ?: 0
 
-        val (pet, shay) = if (level == 1) routine(petCount, shayCount) else equalization(petCount, shayCount)
+        val (product, pet, shay) = if (level != 1 && petCount != 0 && shayCount != 0)
+            equalization(petCount, shayCount)
+        else
+            routine(petCount, shayCount)
         val inerSet = material.filterNot { it is PetIner }.toMutableSet()
-        ShayIner.init(shay)
         val extra = if (level == 3) 5 else 0
-        inerSet.add(PetInerI(pet, extra))
+        // 更新产物
+        ShayIner.init(shay)
+        inerSet.add(PetIner(pet))
+        inerSet.add(PetInerI(product, extra))
         return inerSet
     }
 }
@@ -41,11 +53,16 @@ class DopingII(override val level: Int) : Doping() {
         val petCount = material.find { it is PetInerI }?.count ?: 0
         val gabeCount = material.find { it is GabeInerI }?.count ?: 0
 
-        val (pet, gabe) = if (level == 3) equalization(petCount, gabeCount) else routine(petCount, gabeCount)
+        val (product, pet, gabe) = if (level == 3 && petCount != 0 && gabeCount != 0)
+            equalization(petCount, gabeCount)
+        else
+            routine(petCount, gabeCount)
         val inerSet = material.filterNot { it is PetInerI || it is GabeInerI }.toMutableSet()
-        inerSet.add(GabeInerI(gabe))
         val extra = if (level >= 2) 15 else 0
-        inerSet.add(PetInerII(pet, extra))
+        // 更新产物
+        inerSet.add(GabeInerI(gabe))
+        inerSet.add(PetInerI(pet))
+        inerSet.add(PetInerII(product, extra))
         return inerSet
     }
 }
@@ -57,10 +74,17 @@ class DopingIII(override val level: Int) : Doping() {
         val petCount = material.find { it is PetInerII }?.count ?: 0
         val nesreCount = material.find { it is NesreInerIII }?.count ?: 0
 
-        val (pet, nesre) = if (level == 1) routine(petCount, nesreCount) else equalization(petCount, nesreCount)
+        val (product, pet, nesre) = if (level != 1 && petCount != 0 && nesreCount != 0)
+            equalization(petCount, nesreCount)
+        else
+            routine(petCount, nesreCount)
         val inerSet = material.filterNot { it is PetInerII || it is NesreInerIII }.toMutableSet()
+
+        // 更新产物
         inerSet.add(NesreInerIII(nesre))
-        inerSet.add(PetInerIII(pet, level == 3))
+        inerSet.add(PetInerII(pet))
+        inerSet.add(PetInerIII(product, level == 3))
+
         return inerSet
     }
 }
